f4a596d17132ed944ca88904263bb5806c436d1c
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2019-10-09 15:44:04 -0400

    bug fix

--- includes/controllers/api.php
+++ includes/controllers/api.php
@@ -590,7 +590,7 @@
                 $return['status'] = "400";
                 $return['message'] = "Bad Request";
             }
-            print_r($return);
+            //print_r($return);
             return $this->convert_to_json($return);
         }
 
